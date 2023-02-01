31d64f2291e9b4a3c5d9b6c15ad98b8f4cd7b490
Joseph Martin <josephmartin1350@gmail.com>
2023-01-31 21:21:27 -0700

    commit tree f9dc7fabf9eba5882276bb6ca930d17243f9894b

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,17 +7,20 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
@@ -25,7 +28,11 @@ class ToysController < ApplicationController
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:id, :name, :image, :likes)
+  end
+
+  def find_toy
+    toy = Toy.find(params[:id])
   end
 
 end
